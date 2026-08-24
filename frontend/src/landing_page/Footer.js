11f76acc62477fc7aa464a6d5b4eb72dc4f6f343
Durgesh Nandan <durgeshnandan748@gmail.com>
2026-08-25 00:39:03 +0530

    adding landing page section

---
+++ frontend/src/landing_page/Footer.js
@@ -0,0 +1,147 @@
+import React from 'react';
+function Footer() {
+    return (  
+        <div style={{backgroundColor:"#FBFBFB", borderTop: "1px solid #ddd"}}>
+        <div className='container  mt-5'>
+            <div className='row mt-5'>
+                <div className="col">
+                    <img src="media/images/FinSpirax.png" style={{width:"80%"}}/>
+                    <p className='mt-3 text-muted'>© 2026, FINSPIRAX Broking Ltd.
+                     All rights reserved.</p>
+                     <ul className="social">
+    <li>
+        <a href="">
+            <i className="fa-brands fa-x-twitter"></i>
+        </a>
+    </li>
+
+    <li>
+        <a href="">
+            <i className="fa-brands fa-facebook"></i>
+        </a>
+    </li>
+
+    <li>
+        <a href="">
+            <i className="fa-brands fa-instagram"></i>
+        </a>
+    </li>
+
+    <li>
+        <a href="">
+            <i className="fa-brands fa-linkedin"></i>
+        </a>
+    </li>
+</ul>
+
+<hr />
+
+<ul className="social">
+    <li>
+        <a href="">
+            <i className="fa-brands fa-youtube"></i>
+        </a>
+    </li>
+
+    <li>
+        <a href="">
+            <i className="fa-brands fa-whatsapp"></i>
+        </a>
+    </li>
+
+    <li>
+        <a href="">
+            <i className="fa-brands fa-telegram"></i>
+        </a>
+    </li>
+</ul>
+                </div>
+                <div className="col">
+                    <p>Account</p>
+                    <a href="" className="d-block mb-2 footer-link"
+>Open demat account</a>
+                    <a href="" className="d-block mb-2 footer-link">Minor demat account</a>
+                    <a href="" className="d-block mb-2 footer-link">NRI demat account</a>
+                    <a href="" className="d-block mb-2 footer-link">HUF demat account</a>
+                    <a href="" className="d-block mb-2 footer-link">Commodity</a>
+                    <a href="" className="d-block mb-2 footer-link">Dematerialisation</a>
+                    <a href="" className="d-block mb-2 footer-link">Fund transfer</a>
+                    <a href="" className="d-block mb-2 footer-link">MTF</a>
+                </div>
+                <div className="col">
+                    <p>Support</p>
+                    <a href=""  className="d-block mb-2 footer-link">Contact us</a>
+                    <a href=""  className="d-block mb-2 footer-link">Support portal
+                    </a>
+                    <a href=""  className="d-block mb-2 footer-link">How to file a complaint?</a>
+                    <a href=""  className="d-block mb-2 footer-link">Status of your complaints</a>
+                    <a href=""  className="d-block mb-2 footer-link">Bulletin</a>
+                    <a href=""  className="d-block mb-2 footer-link">Circular</a>
+                    <a href=""  className="d-block mb-2 footer-link">F-Connect blog</a>
+                    <a href=""  className="d-block mb-2 footer-link">Downloads</a>
+                </div>
+                <div className="col">
+                    <p>Company</p>
+                    <a href="" className="d-block mb-2 footer-link">About</a>
+                    <a href="" className="d-block mb-2 footer-link">Philosophy
+                    </a>
+                    <a href="" className="d-block mb-2 footer-link">Press & media</a>
+                    <a href="" className="d-block mb-2 footer-link">Careers
+</a>
+                    <a href=""  className="d-block mb-2 footer-link">Finspirax Cares (CSR)</a>
+                    <a href=""  className="d-block mb-2 footer-link">Finspirax.tech</a>
+                    <a href=""  className="d-block mb-2 footer-link">Open source</a>
+                    <a href=""  className="d-block mb-2 footer-link">Referral program</a>
+                </div>
+                <div className="col">
+                    <p>Quick links</p>
+                    <a href="" className="d-block mb-2 footer-link">Upcoming IPOs</a>
+                    <a href="" className="d-block mb-2 footer-link">Brokerage charges
+                    </a>
+                    <a href="" className="d-block mb-2 footer-link">Market holidays</a>
+                    <a href="" className="d-block mb-2 footer-link">Economic calendar</a>
+                    <a href="" className="d-block mb-2 footer-link">Calculators</a>
+                    <a href="" className="d-block mb-2 footer-link">Markets</a>
+                    <a href="" className="d-block mb-2 footer-link">Sectors</a>
+                    <a href="" className="d-block mb-2 footer-link">Gift Nifty</a>
+                </div>
+            </div>
+            <div className="mt-5 text-muted" style={{fontSize:"12px"}}>
+            <p> Finspirax Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Finspirax Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Finspirax Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@Finspirax.com, for DP related to dp@Finspirax.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF.
+            </p>
+<p>
+Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances.</p>
+
+<p>Smart Online Dispute Resolution | Grievances Redressal Mechanism.</p>
+
+<p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
+
+<p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+
+<p>India's largest broker based on networth as per NSE. NSE broker factsheet.</p>
+
+<p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers/depository participants. Receive information of your transactions directly from Exchange/Depositories on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Finspirax and offering such services, please create a ticket here.</p>
+
+<p>*Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.</p>
+
+<p>Fixed deposit products offered on this platform are third-party products (TPP) and are not Exchange traded products. These are offered through Blostem Fintech Private Limited. Finspirax Broking Limited (SEBI Registration No.: INZ000031633) is acting solely as a distributor for these products. Any disputes arising with respect to such distribution activity will not have access to SEBI SCORES/ODR, Exchange Investor Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits are regulated by the Reserve Bank of India (RBI).</p>
+</div>
+<div className="mt-3" style={{fontSize:"14px"}}>
+<p className='text-center '><a href="" className="me-3 footer-link">NSE</a>
+    <a href="" className="me-3 footer-link">BSE</a>
+    <a href="" className="me-3 footer-link">MCX</a>
+    <a href="" className="me-3 footer-link">MSEI</a>
+    <a href="" className="me-3 footer-link">Terms & conditions</a>
+    <a href="" className="me-3 footer-link">Policies & procedures</a>
+    <a href="" className="me-3 footer-link">Privacy policy</a>
+    <a href="" className="me-3 footer-link">Disclosure</a>
+    <a href="" className="me-3 footer-link">For investor's attention</a>
+    <a href="" className="me-3 footer-link">Investor charter</a>
+    <a href="" className="me-3 footer-link">Sitemap</a></p>
+</div>
+</div>
+</div>
+    );
+}
+
+export default Footer;
